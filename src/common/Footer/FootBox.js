54909673beab0aa41299f1da0db7ef2cb55df6f3
KIM-YOUNGSUN <sgi01272@gmail.com>
2022-01-24 18:27:11 +0900

    commit: updated portfolio

--- src/common/Footer/FootBox.js
+++ src/common/Footer/FootBox.js
@@ -1,9 +1,15 @@
 import '../../style/FootBox.scss';
 
 export default function FootBox() {
+  
   return (
     <footer id="footBox">
-      
+      <div className='foot_area'>
+      <p>thank you!</p>
+      <p>읽어주셔서 감사합니다.</p>
+      <button className='btn'>
+      </button>
+      </div>
     </footer>
   )
 }
--- src/page/Main/ViewBox.js
+++ src/page/Main/ViewBox.js
@@ -4,7 +4,7 @@ import '../../style/ViewBox.scss';
 export default function ViewBox() {
   return (
     <article id="viewBox">
-      <h2>youngsun's portifolio</h2>
+      <h2>youngsun's portfolio</h2>
       <div className="view_area"></div>
       <div className="view_text">
         <p>안녕하세요 프론트엔드 개발자 김영선입니다.<br /></p>
